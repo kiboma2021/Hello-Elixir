e050a278fcbc21b9d713eeec1ba3effb5e09dcda
kiboma <omayioben@gmail.com>
2023-01-09 18:37:13 +0300

    Add another template

--- lib/hello_web/controllers/hello.controller.ex
+++ lib/hello_web/controllers/hello.controller.ex
@@ -4,4 +4,8 @@ defmodule HelloWeb.HelloController do
     render(conn, "index.html")
   end
 
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger:messenger)
+  end
+
 end
